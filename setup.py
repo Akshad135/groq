2f15e7e5d99bae8ad283814938b9fb6a62c42f8c
Akshad135 <akshad.agrawal1978@gmail.com>
2024-04-27 19:18:08 +0530

    made commands for changing secrets and updated setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'groq',
-        'python-dotenv',
+        'groq'
     ],
     entry_points={
         'console_scripts': [
             'grc=groq_cli_chat.work:main'
         ],
-    }
+    },
+    description='A CLI package for interacting with the Groq API.',
+    author='Akshad Agrawal',
+    url='https://github.com/Akshad135/groq-cli',
 )
