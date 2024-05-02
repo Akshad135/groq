b49816556373abf0b60d0db0de30c14aa91ebc53
Akshad135 <akshad.agrawal1978@gmail.com>
2024-05-02 11:05:24 +0530

    added long-description

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+long_description = """
+This is a basic CLI package that allows you to interact with the Groq API.
+It provides different options to select the role type (the way which the model will interact) and
+model type (the model which will be used to solve your query).
+"""
+
 setup(
     name='groq-cli',
     version='0.1',
@@ -14,6 +20,8 @@ setup(
         ],
     },
     description='A CLI package for interacting with the Groq API.',
+    long_description=long_description,
+    long_description_content_type='text/plain',
     author='Akshad Agrawal',
     url='https://github.com/Akshad135/groq-cli',
 )
